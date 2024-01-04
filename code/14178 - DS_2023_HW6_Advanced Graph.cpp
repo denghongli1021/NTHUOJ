52366c2b2631e86fb252269bab1b02c3c66444db
denghongli1021 <116105955+denghongli1021@users.noreply.github.com>
2024-01-04 20:44:28 +0800

    Update 14178 - DS_2023_HW6_Advanced Graph.cpp

--- code/14178 - DS_2023_HW6_Advanced Graph.cpp
+++ code/14178 - DS_2023_HW6_Advanced Graph.cpp
@@ -1,92 +1,79 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
-
-struct Edge {
-    int u, v, w;
-    int includedCount;  // New field to track inclusion count
-};
-
-bool compareEdges(const Edge& e1, const Edge& e2) {
-    return e1.w < e2.w;
-}
 
 class UnionFind {
 public:
-    UnionFind(int n) {
-        parent.resize(n);
-        rank.resize(n, 1);
-        for (int i = 0; i < n; ++i) {
-            parent[i] = i;
-        }
+  vector<int> p;
+  int n;
+
+  UnionFind(int _n)
+    : n(_n)
+    , p(_n) {
+    iota(p.begin(), p.end(), 0);
+  }
+
+  bool unite(int a, int b) {
+    if (find(a) == find(b)) return false;
+    p[find(a)] = find(b);
+    --n;
+    return true;
+  }
+
+  int find(int x) {
+    if (p[x] != x) p[x] = find(p[x]);
+    return p[x];
+  }
+};
+
+class Solution {
+public:
+  vector<vector<int>> findCriticalAndPseudoCriticalEdges(int n, vector<vector<int>>& edges) {
+    for (int i = 0; i < edges.size(); ++i) edges[i].push_back(i);
+    sort(edges.begin(), edges.end(), [](auto & a, auto & b) { return a[2] < b[2]; });
+    int v = 0;
+    UnionFind uf(n);
+    for (auto& e : edges) {
+      int f = e[0], t = e[1], w = e[2];
+      if (uf.unite(f, t)) v += w;
     }
+    vector<vector<int>> ans(2);
+    for (auto& e : edges) {
+      int f = e[0], t = e[1], w = e[2], i = e[3];
+      UnionFind ufa(n);
+      int k = 0;
+      for (auto& ne : edges) {
+        int x = ne[0], y = ne[1], z = ne[2], j = ne[3];
+        if (j != i && ufa.unite(x, y)) k += z;
+      }
+      if (ufa.n > 1 || (ufa.n == 1 && k > v)) {
+        ans[0].push_back(i);
+        continue;
+      }
 
-    int find(int x) {
-        if (parent[x] != x) {
-            parent[x] = find(parent[x]);
-        }
-        return parent[x];
+      UnionFind ufb(n);
+      ufb.unite(f, t);
+      k = w;
+      for (auto& ne : edges) {
+        int x = ne[0], y = ne[1], z = ne[2], j = ne[3];
+        if (j != i && ufb.unite(x, y)) k += z;
+      }
+      if (k == v) ans[1].push_back(i);
     }
-
-    void unionSets(int x, int y) {
-        int rootX = find(x);
-        int rootY = find(y);
-
-        if (rootX != rootY) {
-            if (rank[rootX] < rank[rootY]) {
-                parent[rootX] = rootY;
-            } else if (rank[rootX] > rank[rootY]) {
-                parent[rootY] = rootX;
-            } else {
-                parent[rootX] = rootY;
-                rank[rootY]++;
-            }
-        }
-    }
-
-private:
-    vector<int> parent;
-    vector<int> rank;
+    cout << v << '\n';
+    return ans;
+  }
 };
 
 int main() {
-    int N, E;
-    cin >> N >> E;
-
-    vector<Edge> edges(E);
-    for (int i = 0; i < E; ++i) {
-        cin >> edges[i].u >> edges[i].v >> edges[i].w;
-        edges[i].includedCount = 0;  // Initialize inclusion count
-    }
-
-    sort(edges.begin(), edges.end(), compareEdges);
-
-    UnionFind uf(N);
-    int totalCost = 0;
-    int essentialLinks = 0;
-    int secondaryLinks = 0;
-
-    for (Edge& edge : edges) {
-        int rootU = uf.find(edge.u);
-        int rootV = uf.find(edge.v);
-
-        if (rootU != rootV) {
-            uf.unionSets(rootU, rootV);
-            totalCost += edge.w;
-            if (edge.includedCount == 0) {
-                essentialLinks++;
-            }
-        } else if (edge.includedCount == 0) {
-            secondaryLinks++;
-        }
-        edge.includedCount = 1;  // Mark as included in the current minimal-cost route
-    }
-
-    cout << totalCost << endl;
-    cout << essentialLinks << endl;
-    cout << secondaryLinks << endl;
-
-    return 0;
+  ios::sync_with_stdio(false), cin.tie(nullptr);
+  Solution s;
+  int n, m; cin >> n >> m;
+  vector<vector<int>> v(m);
+  for (int i = 0; i < m; i++) {
+    vector<int> tmp(3);
+    for (auto& x : tmp) cin >> x;
+    v[i] = tmp;
+  }
+  auto ans = s.findCriticalAndPseudoCriticalEdges(n, v);
+  cout << ans[0].size() << '\n' << ans[1].size() << '\n';
 }
